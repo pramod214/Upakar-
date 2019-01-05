ef028380f4d3b2d5adfebbe9fbad74c05308dea0
Pramod Neupane <pramod.neupane21@gmail.com>
2019-01-05 13:38:25 +0545

    Login Page

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::match(['get','post'],'/adminlogin','AdminController@login')->name('admin.login');
+
+
